7bb2b16901bf586afe460b8789219887996cba4c
Mark Kravinskiy <mark@kravinskiy.com>
2020-01-17 23:52:16 +0100

    feat: Add conversion to Entities model at EntitiesRepository from JSON

--- app/Entities/Repositories/EntitiesRepository.php
+++ app/Entities/Repositories/EntitiesRepository.php
@@ -2,9 +2,34 @@
 
 namespace App\Entities\Repositories;
 
+use App\Entities\Models\Entity;
 use App\Platform\Repositories\JsonRepository;
+use Illuminate\Support\Collection;
 
 class EntitiesRepository extends JsonRepository
 {
+    public function __construct()
+    {
+        $this->setJsonFile(base_path('composer.json'));
+    }
 
+    /**
+     * @return Collection
+     */
+    public function handle(): Collection
+    {
+        $entities = parent::handle();
+
+        $entities->each(function (&$entity) {
+            $entity = (new Entity())
+                        ->setId($entity->id)
+                        ->setEntityType($entity->entity_type)
+                        ->setAddress($entity->address)
+                        ->setCreditScore($entity->credit_score)
+                        ->setProbabilityDefault($entity->probability_default)
+                        ->setProbabilityLoan($entity->probability_loan);
+        });
+
+        return $entities;
+    }
 }
