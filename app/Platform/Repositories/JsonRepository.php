4d178f335d6b312379c89cd8afed3cccd17147aa
Mark Kravinskiy <mark@kravinskiy.com>
2020-01-17 23:51:27 +0100

    feat: Add logic to JSON abstract repository

--- app/Platform/Repositories/JsonRepository.php
+++ app/Platform/Repositories/JsonRepository.php
@@ -2,7 +2,39 @@
 
 namespace App\Platform\Repositories;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+
 abstract class JsonRepository extends AbstractRepository
 {
+    /**
+     * @var string
+     */
+    private $jsonFile;
 
+    /**
+     * @return string
+     */
+    public function getJsonFile(): string
+    {
+        return $this->jsonFile;
+    }
+
+    /**
+     * @param string $jsonFile
+     * @return JsonRepository
+     */
+    public function setJsonFile(string $jsonFile): JsonRepository
+    {
+        $this->jsonFile = $jsonFile;
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function handle(): Collection
+    {
+        return collect(json_decode(File::get($this->jsonFile)));
+    }
 }
