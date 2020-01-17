acb81cd3df2ee9e46c65466128f79d87b4010933
Mark Kravinskiy <mark@kravinskiy.com>
2020-01-18 00:15:27 +0100

    feat: Filter only entities with correct name in GetEntitiesByNameRepository

--- app/Entities/Repositories/GetEntitiesByNameRepository.php
+++ app/Entities/Repositories/GetEntitiesByNameRepository.php
@@ -2,9 +2,33 @@
 
 namespace App\Entities\Repositories;
 
+use App\Entities\Models\Entity;
 use App\Platform\Repositories\AbstractRepository;
 
 class GetEntitiesByNameRepository extends AbstractRepository
 {
+    /**
+     * @var EntitiesRepository
+     */
+    private $entitiesRepository;
 
+    public function __construct(EntitiesRepository $entitiesRepository)
+    {
+        $this->entitiesRepository = $entitiesRepository;
+    }
+
+    /**
+     * @param string $entity_name
+     * @return Entity|null
+     */
+    public function handle(string $entity_name): ?Entity
+    {
+        return $this->entitiesRepository->handle()->first(function(Entity $entity) use ($entity_name) {
+            if ($entity->getEntityName() == $entity_name) {
+                return true;
+            }
+
+            return false;
+        });
+    }
 }
