8c1335917a68df8479be52350c563275405aba66
Mark Kravinskiy <mark@kravinskiy.com>
2020-01-17 23:07:27 +0100

    feat: Add default transformation for entity info

--- app/Entities/Transformations/EntityInfoTransformation.php
+++ app/Entities/Transformations/EntityInfoTransformation.php
@@ -1,15 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kravinskiy
- * Date: 17/01/2020
- * Time: 21:44
- */
 
 namespace App\Entities\Transformations;
 
 
 class EntityInfoTransformation
 {
+    public function handle() {
 
+    }
 }
