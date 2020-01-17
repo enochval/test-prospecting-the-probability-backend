1edc7d7f44a7d0e403adb4063237fc3ceb89ad83
Mark Kravinskiy <mark@kravinskiy.com>
2020-01-18 00:14:38 +0100

    feat: Add GetEntityByNameService logic

--- app/Entities/Services/GetEntitiesByNameService.php
+++ app/Entities/Services/GetEntitiesByNameService.php
@@ -2,9 +2,22 @@
 
 namespace App\Entities\Services;
 
+use App\Entities\Models\Entity;
+use App\Entities\Repositories\GetEntitiesByNameRepository;
 use App\Platform\Services\AbstractService;
 
 class GetEntitiesByNameService extends AbstractService
 {
+    public function __construct(GetEntitiesByNameRepository $getEntitiesByNameRepository)
+    {
+        $this->setRepository($getEntitiesByNameRepository);
+    }
 
+    /**
+     * @param string $entity_name
+     * @return Entity|null
+     */
+    public function handle(string $entity_name): ?Entity {
+        return $this->getRepository()->handle($entity_name);
+    }
 }
